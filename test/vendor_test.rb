c9038a4ab038f92d6a62b7dc216f7ef6db88b0b1
Dani Coleman <danicoleman00@gmail.com>
2020-07-29 09:46:46 -0600

    Add vendors and items to setup and add potential revenue test

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -6,64 +6,58 @@ require './lib/vendor'
 class VendorTest < Minitest::Test
 
   def setup
-    @vendor = Vendor.new("Rocky Mountain Fresh")
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor3 = Vendor.new("Palisade Peach Shack")
     @item1 = Item.new({name: 'Peach', price: "$0.75"})
     @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @item3   = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4   = Item.new({name: "Banana Nice Cream", price: "$4.25"})
   end
 
   def test_it_exists
-    vendor = Vendor.new("Rocky Mountain Fresh")
-    assert_instance_of Vendor, vendor
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    assert_instance_of Vendor, @vendor1
   end
 
   def test_it_has_attributes
-    assert_equal "Rocky Mountain Fresh", @vendor.name
-    assert_equal ({}), @vendor.inventory
+    assert_equal "Rocky Mountain Fresh", @vendor1.name
+    assert_equal ({}), @vendor1.inventory
   end
 
   def test_is_start_with_no_stock
-    assert_equal 0, @vendor.check_stock(@item1)
+    assert_equal 0, @vendor1.check_stock(@item1)
   end
 
   def test_can_stock_items
-    assert_equal 0, @vendor.check_stock(@item1)
-    @vendor.stock(@item1, 30)
-    assert_equal 30, @vendor.check_stock(@item1)
-    @vendor.stock(@item1, 25)
-    assert_equal 55, @vendor.check_stock(@item1)
+    assert_equal 0, @vendor1.check_stock(@item1)
+    @vendor1.stock(@item1, 30)
+    assert_equal 30, @vendor1.check_stock(@item1)
+    @vendor1.stock(@item1, 25)
+    assert_equal 55, @vendor1.check_stock(@item1)
+    @vendor1.stock(@item2, 12)
+    assert_equal 12, @vendor1.check_stock(@item2)
   end
 
+  def test_its_inventory
+    @vendor1.stock(@item1, 30)
+    @vendor1.stock(@item1, 25)
+    @vendor1.stock(@item2, 12)
+    expected = {
+      @item1 => 55,
+      @item2 => 12
+    }
+    assert_equal expected, @vendor1.inventory
+  end
+
+  def test_it_has_a_potential_revenue
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+    assert_equal 29.75, @vendor1.potential_revenue
+    assert_equal 345.00, @vendor2.potential_revenue
+    assert_equal 48.75, @vendor3.potential_revenue
+  end
 end
-
-
-
-
-
-
-
-
-
-
-# #=> #<Item:0x007f9c565c0ce8...>
-# item2.name
-# #=> "Tomato"
-# item2.price
-# #=> 0.5
-# #=> #<Vendor:0x00007f85683152f0...>
-# vendor.name
-# #=> "Rocky Mountain Fresh"
-# vendor.inventory
-# #=> {}
-# vendor.check_stock(item1)
-# #=> 0
-# vendor.stock(item1, 30)
-# vendor.inventory
-# #=> {#<Item:0x007f9c56740d48...> => 30}
-# vendor.check_stock(item1)
-# #=> 30
-# vendor.stock(item1, 25)
-# vendor.check_stock(item1)
-# #=> 55
-# vendor.stock(item2, 12)
-# vendor.inventory
-#=> {#<Item:0x007f9c56740d48...> => 55, #<Item:0x007f9c565c0ce8...> => 12}
