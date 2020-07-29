c4aa5684c7ce6ebe824227ca6066a157c1a167f6
Dani Coleman <danicoleman00@gmail.com>
2020-07-29 10:16:44 -0600

    Add final iteration 2 test

--- test/market_test.rb
+++ test/market_test.rb
@@ -38,4 +38,30 @@ class MarketTest < Minitest::Test
     assert_equal [@vendor1, @vendor2, @vendor3], @market.vendors
   end
 
+  def test_can_get_vendors_by_name
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+    expected = ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
+    assert_equal expected, @market.vendor_names
+  end
+
+  def test_can_get_vendors_that_sell
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+    assert_equal [@vendor1, @vendor3], @market.vendors_that_sell(@item1)
+    assert_equal [@vendor2], @market.vendors_that_sell(@item4)
+  end
+
 end
