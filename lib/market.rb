1cd2013909252ffb88e1a6b1d716229525f5a058
Dani Coleman <danicoleman00@gmail.com>
2020-07-29 10:39:42 -0600

    Add total_inventory and overstocked_items methods

--- lib/market.rb
+++ lib/market.rb
@@ -22,4 +22,34 @@ class Market
       vendor.check_stock(item) > 0
     end
   end
+
+  def sorted_item_list
+    sorted_items = []
+    @vendors.each do |vendor|
+      vendor.all_items_in_stock.each do |item|
+        sorted_items << item.name
+      end
+    end
+    sorted_items.uniq.sort
+  end
+
+  def total_inventory
+    result = Hash.new
+    @vendors.each do |vendor|
+      vendor.inventory.each do |item, quantity|
+        quantity_sum = vendors_that_sell(item).sum do |vendor|
+          vendor.inventory[item]
+        end
+        result[item] = {quantity: quantity_sum, vendors: vendors_that_sell(item)}
+      end
+    end
+    result
+  end
+
+  def overstocked_items
+    found = total_inventory.select do |item, data|
+       data[:quantity] > 50 &&  data[:vendors].count > 1
+     end
+     found.keys
+  end
 end
